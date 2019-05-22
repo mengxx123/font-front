3cbd2e8071d8f461b0d81ff27ddaa0917939597d
yunser <>
2019-05-22 22:58:29 +0800

    backup

--- src/config/index.js
+++ src/config/index.js
@@ -2,10 +2,11 @@ let debug = process.env.NODE_ENV !== 'production'
 
 let apiDomain
 if (process.env.NODE_ENV === 'production') {
-    apiDomain = 'https://fontapi.yunser.com'
+    // apiDomain = 'https://fontapi.yunser.com'
+    apiDomain = 'https://nodeapi.yunser.com'
 } else {
-    // apiDomain = 'http://localhost:1027'
-    apiDomain = 'https://fontapi.yunser.com'
+    apiDomain = 'http://localhost:1026'
+    // apiDomain = 'https://fontapi.yunser.com'
 }
 
 module.exports = {
